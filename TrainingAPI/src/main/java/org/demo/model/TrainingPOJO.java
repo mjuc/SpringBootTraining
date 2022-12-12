8dab65006633bcea03d1d5d406dc2ac164358ec4
mjuc <michal.jucewicz@protonmail.com>
2022-12-12 19:18:53 +0100

    Added attributes and constructor

--- TrainingAPI/src/main/java/org/demo/model/TrainingPOJO.java
+++ TrainingAPI/src/main/java/org/demo/model/TrainingPOJO.java
@@ -1,2 +1,13 @@
-package org.demo.model;public class TrainingPOJO {
+package org.demo.model;
+
+public class TrainingPOJO {
+    private int id;
+    private String type;
+    private String date;
+
+    public TrainingPOJO(int id, String type, String date) {
+        this.id = id;
+        this.type = type;
+        this.date = date;
+    }
 }
