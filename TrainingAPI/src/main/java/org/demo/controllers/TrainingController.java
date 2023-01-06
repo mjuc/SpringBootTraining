992c53501141dd277f695bcdfb2bdbbe2f8a906d
mjuc <michal.jucewicz@protonmail.com>
2023-01-06 16:49:13 +0100

    Added endpoint to add new session

--- TrainingAPI/src/main/java/org/demo/controllers/TrainingController.java
+++ TrainingAPI/src/main/java/org/demo/controllers/TrainingController.java
@@ -3,11 +3,15 @@ package org.demo.controllers;
 import org.demo.entity.TrainingPOJO;
 import org.demo.services.TrainingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(path = "/trainings")
@@ -20,6 +24,17 @@ public class TrainingController {
     }
     @GetMapping("/")
     public TrainingPOJO last(){
-        return new TrainingPOJO(1,"","");
+        return trainingService.last();
+    }
+    @PostMapping(value = "/add",consumes = "application/json")
+    public ResponseEntity<Object> add(@RequestBody String type){
+        //Integer id = trainingService.allSessions().size() + 1;
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        String date = format.format(new Date());
+        TrainingPOJO training = new TrainingPOJO(type,date);
+        trainingService.add(training);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().buildAndExpand(training.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
