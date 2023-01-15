bafeb765355f2a6b553d7462ae913ff71eb46bbc
mjuc <michal.jucewicz@protonmail.com>
2023-01-15 10:45:04 +0100

    Added dto for type url

--- TrainingAPI/src/main/java/org/demo/request/TrainingType.java
+++ TrainingAPI/src/main/java/org/demo/request/TrainingType.java
@@ -1,2 +1,16 @@
-package org.demo.request;public class TrainingType {
+package org.demo.request;
+public class TrainingType {
+    private String type;
+
+    public TrainingType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
