6ef0970a2ca488c27ad8f4fb683a6bcfc61659f7
mjuc <michal.jucewicz@protonmail.com>
2023-01-17 16:50:55 +0100

    Added mockito framework to dependecies

---
+++ TrainingAPI/src/test/java/org/demo/entity/TrainingTest.java
@@ -0,0 +1,31 @@
+package org.demo.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TrainingTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getDate() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void setDate() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setType() {
+    }
+}
