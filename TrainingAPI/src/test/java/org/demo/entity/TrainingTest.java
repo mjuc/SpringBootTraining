8b146b89024d5f07c6e5994887fa1f5cdee94d3c
mjuc <michal.jucewicz@protonmail.com>
2023-01-17 16:51:24 +0100

    Added unit tests for entity class

--- TrainingAPI/src/test/java/org/demo/entity/TrainingTest.java
+++ TrainingAPI/src/test/java/org/demo/entity/TrainingTest.java
@@ -2,30 +2,24 @@ package org.demo.entity;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TrainingTest {
 
     @Test
-    void getId() {
-    }
-
-    @Test
     void getDate() {
+        var now = LocalDateTime.now();
+        var training = new Training("test",now);
+        assertEquals(training.getDate(),now);
     }
 
     @Test
     void getType() {
+        var now = LocalDateTime.now();
+        var training = new Training("test",now);
+        assertEquals(training.getType(),"test");
     }
 
-    @Test
-    void setDate() {
-    }
-
-    @Test
-    void setId() {
-    }
-
-    @Test
-    void setType() {
-    }
 }
