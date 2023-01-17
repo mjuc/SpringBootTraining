27c4da28b98342e4c0b6fe90d17a3cb3d56e4417
mjuc <michal.jucewicz@protonmail.com>
2023-01-17 16:53:06 +0100

    Added test classes for service & controller

---
+++ TrainingAPI/src/test/java/org/demo/controllers/TrainingControllerTest.java
@@ -0,0 +1,7 @@
+package org.demo.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainingControllerTest {
+
+}
---
+++ TrainingAPI/src/test/java/org/demo/services/TrainingServiceTest.java
@@ -0,0 +1,7 @@
+package org.demo.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainingServiceTest {
+
+}
