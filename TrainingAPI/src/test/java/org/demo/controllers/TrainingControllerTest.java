10948abb1c218d8bcad144928c3ee6f023e72188
mjuc <michal.jucewicz@protonmail.com>
2023-01-18 20:39:50 +0100

    Added unit tests for controller component

--- TrainingAPI/src/test/java/org/demo/controllers/TrainingControllerTest.java
+++ TrainingAPI/src/test/java/org/demo/controllers/TrainingControllerTest.java
@@ -1,7 +1,40 @@
 package org.demo.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.demo.entity.Training;
+import org.demo.request.TrainingType;
+import org.demo.services.TrainingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class TrainingControllerTest {
-
+    @Mock private TrainingService trainingService;
+    private TrainingController trainingController;
+    @BeforeEach
+    void setTrainingController(){
+        this.trainingController = new TrainingController(this.trainingService);
+    }
+    @Test
+    void testGetAllSessions(){
+        trainingController.getAllSessions();
+        verify(trainingService).allSessions();
+    }
+    @Test
+    void testLast(){
+        trainingController.last();
+        verify(trainingService).last();
+    }
+    @Test
+    void testGetByType(){
+        trainingController.getByType("test");
+        verify(trainingService).getSessionsByType("test");
+    }
 }
