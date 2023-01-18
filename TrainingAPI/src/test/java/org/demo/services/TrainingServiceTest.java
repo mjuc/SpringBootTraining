fdddf583ed36952bf8a54121fb4f98fc3a5b69f4
mjuc <michal.jucewicz@protonmail.com>
2023-01-18 20:17:36 +0100

    Added unit tests for service component

--- TrainingAPI/src/test/java/org/demo/services/TrainingServiceTest.java
+++ TrainingAPI/src/test/java/org/demo/services/TrainingServiceTest.java
@@ -1,7 +1,43 @@
 package org.demo.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.demo.entity.Training;
+import org.demo.repositories.TrainingRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class TrainingServiceTest {
+    @Mock private TrainingRepository trainingRepository;
+    private TrainingService trainingService;
+    @BeforeEach
+    void setup(){
+        this.trainingService = new TrainingService(this.trainingRepository);
+    }
+    @Test
+    void testGetAll(){
+        trainingService.allSessions();
+        verify(trainingRepository).findAll();
+    }
+    @Test
+    void testGetByType(){
+        trainingService.getSessionsByType("test");
+        verify(trainingRepository).findByType("test");
+    }
+    @Test
+    void  testLast(){
+        trainingService.last();
+        verify(trainingRepository).findTopByOrderByIdDesc();
+    }
+    @Test
+    void testAdd(){
+        Training emptyTraining = mock(Training.class);
+        trainingService.add(emptyTraining);
+        verify(trainingRepository).save(emptyTraining);
+    }
 
 }
