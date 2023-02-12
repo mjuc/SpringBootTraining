8b0c5aa71ef9b54e85cda593bffe15e7a821d070
mjuc <michal.jucewicz@protonmail.com>
2023-02-12 10:13:59 +0100

    Changed GET endpoints to use response dao as return type

--- TrainingAPI/src/main/java/org/demo/controllers/TrainingController.java
+++ TrainingAPI/src/main/java/org/demo/controllers/TrainingController.java
@@ -1,5 +1,6 @@
 package org.demo.controllers;
 
+import org.demo.controllers.response.TrainingResponse;
 import org.demo.entity.Training;
 import org.demo.controllers.request.TrainingType;
 import org.demo.services.TrainingService;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.net.URI;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/trainings")
@@ -21,16 +23,37 @@ public class TrainingController {
     }
 
     @GetMapping("/")
-    public List<Training> getAllSessions(){
-        return trainingService.allSessions();
+    public List<TrainingResponse> getAllSessions(){
+        return trainingService.allSessions().stream().map(
+                t -> {
+                    TrainingResponse r = new TrainingResponse();
+                    r.setId(t.getId());
+                    r.setType(t.getType());
+                    r.setDate(t.getDate());
+                    return r;
+                }
+        ).collect(Collectors.toList());
     }
     @GetMapping("/last")
-    public Training last(){
-        return trainingService.last();
+    public TrainingResponse last(){
+        var last = trainingService.last();
+        var ret = new TrainingResponse();
+        ret.setId(last.getId());
+        ret.setType(last.getType());
+        ret.setDate(last.getDate());
+        return ret;
     }
     @GetMapping("/{type}")
-    public List<Training> getByType(@PathVariable("type") String type){
-        return trainingService.getSessionsByType(type);
+    public List<TrainingResponse> getByType(@PathVariable("type") String type){
+        return trainingService.getSessionsByType(type).stream().map(
+                t -> {
+                    TrainingResponse r = new TrainingResponse();
+                    r.setId(t.getId());
+                    r.setType(t.getType());
+                    r.setDate(t.getDate());
+                    return r;
+                }
+        ).collect(Collectors.toList());
     }
     @PostMapping(value = "/",consumes = "application/json")
     public ResponseEntity<Object> add(@RequestBody TrainingType type){
--- TrainingAPI/src/main/java/org/demo/controllers/response/TrainingResponse.java
+++ TrainingAPI/src/main/java/org/demo/controllers/response/TrainingResponse.java
@@ -1,4 +1,34 @@
 package org.demo.controllers.response;
 
+
+import java.time.LocalDateTime;
+
 public class TrainingResponse {
+    private int id;
+    private String type;
+    private LocalDateTime date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 }
