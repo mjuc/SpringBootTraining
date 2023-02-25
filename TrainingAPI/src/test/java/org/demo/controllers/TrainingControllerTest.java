eb47dd737509a041ba1cdfddf26f2c4e917f563e
mjuc <michal.jucewicz@protonmail.com>
2023-02-25 12:42:06 +0100

    Upgraded unit tests for controller

--- TrainingAPI/src/test/java/org/demo/controllers/TrainingControllerTest.java
+++ TrainingAPI/src/test/java/org/demo/controllers/TrainingControllerTest.java
@@ -1,5 +1,7 @@
 package org.demo.controllers;
 
+import org.demo.controllers.response.TrainingResponse;
+import org.demo.entity.Training;
 import org.demo.services.TrainingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,8 +9,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.mock;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TrainingControllerTest {
@@ -25,12 +32,23 @@ class TrainingControllerTest {
     }
     @Test
     void testLast(){
-        trainingController.last();
+        var date = LocalDateTime.now();
+        when(trainingService.last()).thenReturn(new Training("type",date));
+        var res = trainingController.last();
         verify(trainingService).last();
+        assertEquals(res.getType(),"type");
+        assertEquals(res.getDate(),date);
     }
     @Test
     void testGetByType(){
-        trainingController.getByType("test");
+        when(trainingService.getSessionsByType("test")).thenReturn(
+                List.of(new Training("test", LocalDateTime.now()),
+                        new Training("test", LocalDateTime.now())));
+        var res = trainingController.getByType("test");
         verify(trainingService).getSessionsByType("test");
+        for (TrainingResponse t: res) {
+            assertEquals(t.getType(),"test");
+        }
     }
+    
 }
