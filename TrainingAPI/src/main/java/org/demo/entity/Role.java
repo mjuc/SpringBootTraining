883b055dc470757e9cdeb304cde5c8d72b73ad65
mjuc <michal.jucewicz@protonmail.com>
2023-03-08 13:41:47 +0100

    Added constructors, getters & setters

--- TrainingAPI/src/main/java/org/demo/entity/Role.java
+++ TrainingAPI/src/main/java/org/demo/entity/Role.java
@@ -1,6 +1,7 @@
 package org.demo.entity;
 
 import jakarta.persistence.*;
+import org.demo.models.ERole;
 
 @Entity
 @Table(name = "roles")
@@ -9,4 +10,30 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
 }
