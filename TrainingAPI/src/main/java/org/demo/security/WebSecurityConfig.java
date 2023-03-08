732ca6435d9f7d3b1e0a75b1641dddbd5b4c38ee
mjuc <michal.jucewicz@protonmail.com>
2023-03-08 15:01:25 +0100

    Removed deprecated annotation

--- TrainingAPI/src/main/java/org/demo/security/WebSecurityConfig.java
+++ TrainingAPI/src/main/java/org/demo/security/WebSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,9 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true)
-
+@EnableGlobalAuthentication
 public class WebSecurityConfig {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
