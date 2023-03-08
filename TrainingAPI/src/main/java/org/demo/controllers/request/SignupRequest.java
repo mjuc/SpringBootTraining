4f2a776fee6ae320e512c1424020b0e60f64e042
mjuc <michal.jucewicz@protonmail.com>
2023-03-08 15:35:30 +0100

    Updated signup request to contain field for user roles

--- TrainingAPI/src/main/java/org/demo/controllers/request/SignupRequest.java
+++ TrainingAPI/src/main/java/org/demo/controllers/request/SignupRequest.java
@@ -1,9 +1,12 @@
 package org.demo.controllers.request;
 
+import java.util.Set;
+
 public class SignupRequest {
     private String username;
     private String email;
     private String password;
+    private Set<String> role;
 
     public SignupRequest() {
     }
@@ -19,16 +22,7 @@ public class SignupRequest {
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public Set<String> getRole() {
+        return role;
     }
 }
