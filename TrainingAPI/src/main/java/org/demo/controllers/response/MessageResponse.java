7490ce6e1c45c13073b8f49d0759ae867a170405
mjuc <michal.jucewicz@protonmail.com>
2023-03-20 21:14:15 +0100

    Removed authentication due to dependcies issues. Requires complete rebuild

--- TrainingAPI/src/main/java/org/demo/controllers/response/MessageResponse.java
+++
@@ -1,9 +0,0 @@
-package org.demo.controllers.response;
-
-public class MessageResponse {
-    private String message;
-
-    public MessageResponse(String message) {
-        this.message = message;
-    }
-}
--- TrainingAPI/src/main/java/org/demo/models/ERole.java
+++
@@ -1,7 +0,0 @@
-package org.demo.models;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
