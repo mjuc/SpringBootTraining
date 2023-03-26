0467a9f53068607e1580aacb54779195370b4af5
mjuc <michal.jucewicz@protonmail.com>
2023-03-26 12:51:36 +0200

    Renamed

--- TrainingAPI/src/main/java/org/demo/controllers/request/UserRequest.java
+++ TrainingAPI/src/main/java/org/demo/controllers/request/UserRequest.java
@@ -3,7 +3,7 @@ package org.demo.controllers.request;
 import lombok.Getter;
 
 @Getter
-public class UserRegistration {
+public class UserRequest {
     private String username;
     private String password;
 }
